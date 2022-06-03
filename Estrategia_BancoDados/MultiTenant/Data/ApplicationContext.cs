00610040c16846918bd4e29167f93e07bc50e3b8
Igor de Moraes Silva Ribeiro <igormoraes90@yahoo.com.br>
2022-06-02 22:23:25 -0300

    Correção.

--- Estrategia_BancoDados/MultiTenant/Data/ApplicationContext.cs
+++ Estrategia_BancoDados/MultiTenant/Data/ApplicationContext.cs
@@ -20,13 +20,13 @@ namespace MultiTenant.Data
             modelBuilder.Entity<Person>().HasData(
                 new Person { Id = 1, Name = "Person 1", TenantId = "tenant-1" },
                 new Person { Id = 2, Name = "Person 2", TenantId = "tenant-2" },
-                new Person { Id = 2, Name = "Person 3", TenantId = "tenant-2" }
+                new Person { Id = 3, Name = "Person 3", TenantId = "tenant-2" }
             );
 
             modelBuilder.Entity<Product>().HasData(
                 new Product { Id = 1, Description = "Description 1", TenantId = "tenant-1" },
                 new Product { Id = 2, Description = "Description 2", TenantId = "tenant-2" },
-                new Product { Id = 2, Description = "Description 3", TenantId = "tenant-2" }
+                new Product { Id = 3, Description = "Description 3", TenantId = "tenant-2" }
             );
 
             modelBuilder.Entity<Person>().HasQueryFilter(p => p.TenantId == _tenant.TenantId);
